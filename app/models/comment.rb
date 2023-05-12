c8a7bea02d4d2e0c660a9ba6b0a0911ba56a7769
Ayo Moses <ayomoses111@gmail.com>
2023-05-12 22:10:24 +0100

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  belongs_to :post 
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
   def update_post_comment_counter
     post.increment(:commentscounter)
   end
-
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :post 
-  belongs_to :post class_name: "User"
+  belongs_to :post
+  belongs_to :post, class_name: 'User'
 
   def update_post_like_counter
     post.increment(:likescounter)
