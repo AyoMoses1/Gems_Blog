f3accad989dd15f7b325c5add346efc947c13855
Becky449 <rabnab12@gmail.com>
2023-05-27 10:52:34 +0300

    fix errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,47 @@
-def new
-  @post = @current_user.posts.new
-end
+class PostsController < ApplicationController
+  load_and_authorize_resource
+  before_action :authenticate_user!
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-def create
-  @user = User.find(params[:user_id])
-  @post = @current_user.posts.new(post_params)
-  @post.commentscounter = 0
-  @post.likescounter = 0
-  respond_to do |format|
-    format.html do
-      if @post.save
-        flash[:success] = 'Post was successfully added.'
-        redirect_to user_posts_path(@user)
-      else
-        render :new, locals: { post: @post }
+  def show
+    @post = Post.find(params[:id])
+    @current_user = current_user
+  end
+
+  def new
+    @post = @current_user.posts.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @current_user.posts.new(post_params)
+    @post.commentscounter = 0
+    @post.likescounter = 0
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post was successfully added.'
+          redirect_to user_posts_path(@user)
+        else
+          render :new, locals: { post: @post }
+        end
       end
     end
   end
-end
 
-def destroy
-  authorize! :delete, @post
-  @post.destroy
-  @current_user.posts_counter -= 1
-  redirect_to user_posts_path(current_user), notice: 'Post was successfully deleted.'
-end
+  def destroy
+    authorize! :delete, @post
+    @post.destroy
+    @current_user.posts_counter -= 1
+    redirect_to user_posts_path(current_user), notice: 'Post was successfully deleted.'
+  end
 
-private
+  private
 
-def post_params
-  params.require(:post).permit(:title, :text)
-end
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
